49d240ee74d8c2c1118a64ad8bd08fcdec682d35
Isevich_savage <124886919+Islambek201632838@users.noreply.github.com>
2023-12-12 14:05:54 +0600

    Update keys.py

--- api/keys.py
+++ api/keys.py
@@ -1,2 +1,2 @@
-CHATBOT_SECRET_KEY='Is201632838'
+CHATBOT_SECRET_KEY='qwerty'
 CHATBOT_DATABASE_URI = 'postgresql://default:8VK6hyweTkGx@ep-mute-heart-62488820-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require'
